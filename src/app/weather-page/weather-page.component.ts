255d648fcc9adfcc8a04a811ed87ee98dac0a9d0
Andrey <konohomoro@gmail.com>
2021-01-25 19:06:04 +0900

    очистил weater-page

--- src/app/weather-page/weather-page.component.ts
+++ src/app/weather-page/weather-page.component.ts
@@ -66,39 +66,7 @@ export class WeatherPageComponent implements OnInit {
           console.log(response);
           this.city = response.city.name;
           this.list = response.list;
-          // console.log(response.list);
         });
     });
   }
 }
-
-/////// все параметры Item: ////////
-
-// type Item = {
-//   dt: number;
-//   main: {
-//     temp: number;
-//     feels_like: number;
-//     temp_min: number;
-//     temp_max: number;
-//     pressure: number;
-//     sea_level: number;
-//     grnd_level: number;
-//     humidity: number;
-//     temp_kf: number;
-//   };
-//   weather: [
-//     {
-//       id: number;
-//       main: string;
-//       description: string;
-//       icon: string;
-//     }
-//   ];
-//   clouds: { all: number };
-//   wind: { speed: number; deg: number };
-//   visibility: number;
-//   pop: number;
-//   sys: { pod: string };
-//   dt_txt: string;
-// };
